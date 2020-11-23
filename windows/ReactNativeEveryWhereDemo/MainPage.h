36d5d9dcdcb924a5a2341907e67f9f5094b18653
btav <brian.ft.mit@gmail.com>
2020-11-22 21:00:31 -0500

    added react-native-windows

---
+++ windows/ReactNativeEveryWhereDemo/MainPage.h
@@ -0,0 +1,21 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+
+namespace winrt::ReactNativeEveryWhereDemo::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::ReactNativeEveryWhereDemo::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
+
